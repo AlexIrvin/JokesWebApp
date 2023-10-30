37fc4e2e1ab8d1e36d7294a9a4353fba8538f1b5
Alex Hack-Irvin <65615188+AlexIrvin@users.noreply.github.com>
2023-10-30 18:50:28 -0400

    Updating with List functionality. Search functionality WIP

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -27,6 +27,15 @@ namespace JokesWebApp.Controllers
                           Problem("Entity set 'ApplicationDbContext.Joke'  is null.");
         }
 
+        //GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+/*                _context.Joke != null ? 
+                          View(await _context.Joke.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Joke'  is null.");*/
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
